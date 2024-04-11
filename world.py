2774559c307b6c4504a64b63cefc757e87521567
Yurii Kulikov <kulikobyura@mail.ru>
2024-04-12 00:21:38 +0300

    нашел пример с такси

--- world.py
+++ world.py
@@ -4,10 +4,10 @@ import pygame
 import gymnasium as gym
 from gymnasium import spaces
 from gymnasium.envs.registration import register
-
+from gymnasium.utils.env_checker import check_env
 
 register(
-    id='test',
+    id='test_v0',
     entry_point='world:TestWorld',
 )
 
@@ -27,6 +27,122 @@ class TestWorld(gym.Env):
     def render(self):
         pass
 
+    def _render_gui(self, mode):
+        try:
+            import pygame
+        except ImportError as ex:
+            raise DependencyNotInstalled(
+                "pygame is not installed, run `pip install gymnasium[toy-text]`"
+            ) from ex
+
+        if self.window is None:
+            pygame.init()
+            pygame.display.set_caption("Taxi")
+            if mode == "human":
+                self.window = pygame.display.set_mode(WINDOW_SIZE)
+            elif mode == "rgb_array":
+                self.window = pygame.Surface(WINDOW_SIZE)
+
+        assert (
+                self.window is not None
+        ), "Something went wrong with pygame. This should never happen."
+
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+
+        # подгрузка картинок
+        # подгрузка спрайта такси
+        if self.taxi_imgs is None:
+            file_names = [
+                # ЗАКИНУТЬ ТУТ НЕСКОЛЬКО КАРТИНОК ДЛЯ ЭКРАНА
+                path.join(path.dirname(__file__), "img/cab_front.png"),
+                path.join(path.dirname(__file__), "img/cab_rear.png"),
+                path.join(path.dirname(__file__), "img/cab_right.png"),
+                path.join(path.dirname(__file__), "img/cab_left.png"),
+            ]
+            self.taxi_imgs = [
+                pygame.transform.scale(pygame.image.load(file_name), self.cell_size)
+                for file_name in file_names
+            ]
+
+        # подгрузка спрайта пассажира
+        if self.passenger_img is None:
+            file_name = path.join(path.dirname(__file__), "img/passenger.png")
+            self.passenger_img = pygame.transform.scale(
+                pygame.image.load(file_name), self.cell_size
+            )
+
+        # отель - таргет
+        if self.destination_img is None:
+            file_name = path.join(path.dirname(__file__), "img/hotel.png")
+            self.destination_img = pygame.transform.scale(
+                pygame.image.load(file_name), self.cell_size
+            )
+            self.destination_img.set_alpha(170)
+
+        # ограничители горизонт
+        if self.median_horiz is None:
+            file_names = [
+                path.join(path.dirname(__file__), "img/gridworld_median_left.png"),
+                path.join(path.dirname(__file__), "img/gridworld_median_horiz.png"),
+                path.join(path.dirname(__file__), "img/gridworld_median_right.png"),
+            ]
+            self.median_horiz = [
+                pygame.transform.scale(pygame.image.load(file_name), self.cell_size)
+                for file_name in file_names
+            ]
+
+        # ограничители вертикаль
+        if self.median_vert is None:
+            file_names = [
+                path.join(path.dirname(__file__), "img/gridworld_median_top.png"),
+                path.join(path.dirname(__file__), "img/gridworld_median_vert.png"),
+                path.join(path.dirname(__file__), "img/gridworld_median_bottom.png"),
+            ]
+            self.median_vert = [
+                pygame.transform.scale(pygame.image.load(file_name), self.cell_size)
+                for file_name in file_names
+            ]
+
+        # бэкграунд
+        if self.background_img is None:
+            file_name = path.join(path.dirname(__file__), "img/taxi_background.png")
+            self.background_img = pygame.transform.scale(
+                pygame.image.load(file_name), self.cell_size
+            )
+
+        desc = self.desc
+
+        # заполнение карты
+        for y in range(0, desc.shape[0]):
+            for x in range(0, desc.shape[1]):
+                cell = (x * self.cell_size[0], y * self.cell_size[1])
+                self.window.blit(self.background_img, cell)
+                if desc[y][x] == b"|" and (y == 0 or desc[y - 1][x] != b"|"):
+                    self.window.blit(self.median_vert[0], cell)
+                elif desc[y][x] == b"|" and (
+                    y == desc.shape[0] - 1 or desc[y + 1][x] != b"|"
+                ):
+                    self.window.blit(self.median_vert[2], cell)
+                elif desc[y][x] == b"|":
+                    self.window.blit(self.median_vert[1], cell)
+                elif desc[y][x] == b"-" and (x == 0 or desc[y][x - 1] != b"-"):
+                    self.window.blit(self.median_horiz[0], cell)
+                elif desc[y][x] == b"-" and (
+                    x == desc.shape[1] - 1 or desc[y][x + 1] != b"-"
+                ):
+                    self.window.blit(self.median_horiz[2], cell)
+                elif desc[y][x] == b"-":
+                    self.window.blit(self.median_horiz[1], cell)
+
+        #
+        for cell, color in zip(self.locs, self.locs_colors):
+            color_cell = pygame.Surface(self.cell_size)
+            color_cell.set_alpha(128)
+            color_cell.fill(color)
+            loc = self.get_surf_loc(cell)
+            self.window.blit(color_cell, (loc[0], loc[1] + 10))
+
     def close(self):
         pass
 
@@ -34,3 +150,16 @@ class TestWorld(gym.Env):
         """Это нам не надо походу"""
         pass
 
+
+if __name__ == "__main__":
+    env = gym.make('test_v0', render_mode='human')
+
+    print("Check environment begin")
+    check_env(env.unwrapped)
+    print("Check environment end")
+
+    obs = env.reset()[0]
+
+    for i in range(10):
+        rand_action = env.action_space.sample()
+        obs, reward, terminated, _, _ = env.step(rand_action)
